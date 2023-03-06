827441c06d8a573a008fb72f90ac6797edebf2d8
pranav <addikampranav39@gmail.com>
2023-03-06 14:48:00 +0530

    initial commi t

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app=express();
+const queries= require("./routes/queries")
+
+const mongoose = require("mongoose")
+
+//db connection
+mongoose.connect('mongodb://localhost/dblearning')
+.then(()=>console.log("CONNECTED TO DB"))
+.catch((err)=>console.log(err))
+
+
+
+
+//
+
+
+//using Routes
+app.use("/Allqueries",queries)
+app.use("/addquery",queries)
+app.use("/getQueries",queries)
+
+app.use("/getQueries/names",queries)
+app.listen(3000,()=>{
+    console.log("3000")
+})
+
+
+
+
